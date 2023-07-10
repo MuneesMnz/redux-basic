61f05d6a2c7f8d05576a979011745cbc242fa0b7
mnz <muneeskarakkad62@gmail.com>
2023-07-10 13:43:52 +0530

    multiple reducers

--- multipleReducer.js
+++ multipleReducer.js
@@ -1,5 +1,6 @@
 const redux = require("redux");
 const createStore = redux.legacy_createStore;
+const combineReducers=redux.combineReducers
 
 const BUY_CAKE = "BUY_CAKE";
 const BUY_ICECREAM = "BUY_ICECREAM";
@@ -16,18 +17,26 @@ function buyIcecream() {
     type: BUY_ICECREAM,
   };
 }
-const initialState = {
-  numOfCakes: 10,
-  numOfIcecream:20
-};
+const initialCakeState={
+    numOfCakes:10
+}
+const initialIcecreamState={
+    numOfIcecream:20
+}
 
-const reducer = (state = initialState, action) => {
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
         ...state,
         numOfCakes: state.numOfCakes - 1,
       };
+    default:
+      return state;
+  }
+};
+const icecreamReducer = (state = initialIcecreamState, action) => {
+  switch (action.type) {
     case BUY_ICECREAM:
       return {
         ...state,
@@ -38,7 +47,11 @@ const reducer = (state = initialState, action) => {
   }
 };
 
-const store = createStore(reducer);
+const rootReducer=combineReducers({
+    cake:cakeReducer,
+    icecream:icecreamReducer
+})
+const store = createStore(rootReducer);
 console.log("Initial State", store.getState());
 const unsubscribe = store.subscribe(() =>
   console.log("Updated state", store.getState())
